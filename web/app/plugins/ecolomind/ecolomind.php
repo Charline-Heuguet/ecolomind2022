bf42bbb94ab6360613737be06bdec24a15245b10
Guillaume-Pou <wlg2012@gmail.com>
2022-09-20 20:48:56 +0200

    désactivate security on plugin

--- web/app/plugins/ecolomind/ecolomind.php
+++ web/app/plugins/ecolomind/ecolomind.php
@@ -7,7 +7,9 @@
  * Plugin URI : http://localhost:8000
  */
 
-require __FILE__ ."/security/init.php";
+// Désactivation de la sécurité du plugin qui fait bugger ce dernier a voir avec les backs. a réactiver une fois le fichier sécurité fonctionnel
+/* include plugin_dir_path(__FILE__) . 'security/init.php'; */
+
 
 require __DIR__ . "/vendor/autoload.php";
 
@@ -18,3 +20,4 @@ define("ECOLOMIND_PLUGIN_ENTRY", __FILE__);
  //On instancie la class Plugin qui sera le cerveau des opérations
  $plugin = new Ecolomind\Plugin;
  $plugin->run();
+ ?>
--- web/app/plugins/ecolomind/src/Plugin.php
+++ web/app/plugins/ecolomind/src/Plugin.php
@@ -7,9 +7,27 @@ class Plugin
 
     public function run()
     {
+ //Dans une class, la fonction add_action à besoin de connaitre, en plus du nom de la fonction à exécuté, le contexte d'execution
+ add_action('init', [$this, 'onInit']);
+        
+ //Lorsque l'on active le plugin on effectue l'action 
+ register_activation_hook(ECOLOMIND_PLUGIN_ENTRY, [$this, "onPluginActivation"]);
 
+ //Le callback s'execute à la desactivation du plugin
+ register_deactivation_hook(ECOLOMIND_PLUGIN_ENTRY, [$this, "onPluginDeactivation"]);
         
     }
+    
+    public function onPluginActivation(){
 
+    }
+
+    public function onPluginDeactivation(){
+
+    }
+
+    public function onInit(){
+        
+    }
 }
 
