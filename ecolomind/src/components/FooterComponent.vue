c5c8feacaf81e489e67a2b78309f81aa74dedf66
Charline-Heuguet <charline.heuguet@gmail.com>
2022-09-21 12:44:11 +0200

    Footer Component

--- ecolomind/src/components/FooterComponent.vue
+++ ecolomind/src/components/FooterComponent.vue
@@ -1,1 +1,42 @@
-<!-- Footer  -->
+<template>
+
+    <footer>
+
+        <div>
+
+            <router-link :to="{name: 'qui-sommes-nous'}">
+                Qui sommes-nous?
+            </router-link>
+
+            <router-link :to="{name: 'contact'}">
+                Contactez-nous
+            </router-link>
+
+        </div>
+
+        <img src="@/assets/logo.png" alt="logo ecolomind">        
+
+
+        <div>
+
+            <router-link :to="{name: 'mentions'}"> 
+                Mentions légales
+            </router-link>
+
+            <router-link :to="{name: 'politique-de-confidentialite'}">
+                Politique de confidentialité
+            </router-link>
+
+        </div>
+    
+
+    </footer>
+
+</template>
+
+<script>
+export default {
+    name: 'FooterComponent',
+    
+}
+</script>
