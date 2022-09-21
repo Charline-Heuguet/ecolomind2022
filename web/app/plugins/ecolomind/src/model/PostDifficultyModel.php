eeed06e3c2db0eaf42d815c864c64b863aa7b2b9
Guillaume-Pou <wlg2012@gmail.com>
2022-09-21 19:56:47 +0200

    wip

--- web/app/plugins/ecolomind/src/model/PostDifficultyModel.php
+++ web/app/plugins/ecolomind/src/model/PostDifficultyModel.php
@@ -13,18 +13,20 @@ class PostDifficultyModel
         global $wpdb;
         $this->wpdbDanslemodel = $wpdb;
     }
+
 public function create()
 {    
-        $charset_collate = $this->wpdbDanslemodel->get_charset_collate();
+         $charset_collate = $this->wpdbDanslemodel->get_charset_collate(); 
 
         $sql = "CREATE TABLE ".self::TABLE_NAME." (
-            id mediumint(9) NOT NULL,
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
             post_id mediumint(9) NOT NULL,
             difficulty_id mediumint(9) NOT FULL
         ) $charset_collate;";
 
         $this->wpdbDanslemodel->query($sql);
 }
+
 public function drop(){
         $sql = "DROP TABLE IF EXISTS ". self::TABLE_NAME ;
         $this->wpdbDanslemodel->query($sql);
