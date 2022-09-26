1fe90734d52a4b928b22b0070dfd7a3657a35c9e
Guillaume-Pou <wlg2012@gmail.com>
2022-09-26 23:56:17 +0200

    wip

--- ecolomind/src/views/HomeView.vue
+++ ecolomind/src/views/HomeView.vue
@@ -2,10 +2,20 @@
 
 <template>
 
- <div id= "background">
+<div id="image"> 
+
+  <div id="bureau"></div>
+  <div id="chambre"></div>
+  <div id="cuisine"></div>
+  <div id="entree"></div>
+  <div id="sdb"></div>
 
 
- </div>
+
+</div> 
+  <!-- <div :style="{'background-image':'url(../assets/maison-vide.png)'}"></div> -->
+ <!--  <img src="../assets/maison-vide.png" alt=""> -->
+ 
 </template>
 
 <script>
@@ -17,18 +27,68 @@ export default {
   components: {
    
   }
-  
+ 
  
 }
 </script>
 
 <style lang="scss">
 
-#background {
-  background-image: url("@/assets/maison-vide.png");
-  width: 100px;
-  height: 100px;
+#image {
+  display: grid;
+  background-image: url('../assets/maison_vide_2.png');
+  width: 103em;
+  height: 102.5em;
+  background-repeat: no-repeat;
+} 
+#bureau{
+  background-image: url('../assets/bureau.png');
+  width:100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  grid-column: 3/5;
+  grid-row: 2/5;
+  margin-left: 1.6em;
+  margin-top: 2.5em;
 }
+#chambre{
+  background-image: url('../assets/chambre.png');
+  width: 100%;
+  height:100%;
+  background-repeat: no-repeat;
+  grid-column: 2/5;
+  grid-row: 2/5;
+  margin-left: 0.7em;
+  margin-top: 2.5em;
+  
+}
+#cusine{
+  background-image: url('../assets/cuisine.png');
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  grid-column: 2/3;
+  grid-row: 2/5;
+}
+#entree{
+  background-image: url('../assets/entrée.png');
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  grid-column: 2/5;
+  grid-row: 3/5;
+  margin-bottom: 14em;
+  margin-left: 0.5em;
+}
+#sdb{
+  background-image: url('../assets/sdb.png');
+  width: 200%;
+  height:100%;
+  background-repeat: no-repeat;
+  margin-top: 1.4em;
+  margin-left: 0.6em;
+}
+
 
 
 
