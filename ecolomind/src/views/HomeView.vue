096c63c60f83cb039e4c88445a1fcc3027e5bf1b
Guillaume-Pou <wlg2012@gmail.com>
2022-09-26 12:05:24 +0200

    wip

--- ecolomind/src/views/HomeView.vue
+++ ecolomind/src/views/HomeView.vue
@@ -1,42 +1,20 @@
 <!-- Page d'accueil -->
 
 <template>
-
-<div id="image" :style="backgroundImage">
-
-
-<!-- <img id="bureau" src="@/assets/bureau.png" alt=""> -->
-
-</div>
-  
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 
-
 export default {
   name: 'HomeView',
-  data(){
-    return{ image: "@/assets/maison_vide.png",}
+  components: {
+   
   }
-
- 
 }
 </script>
-
-<style lang="scss" scoped>
-
-#image{
-
-}
-/* #vide{
- width:20%;
- height: 20%; 
-}
-#bureau{
-  width: 10%;
-  height: 10%;
-} */ 
-</style>
