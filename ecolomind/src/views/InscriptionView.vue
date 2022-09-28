964e1b7b45b9e8e3e8bb8b6cc5db9414cc5b96b2
AlexandreQuemeneur <alexandre.quemeneur17@gmail.com>
2022-09-28 18:50:02 +0200

    InscriptionForm try

--- ecolomind/src/views/InscriptionView.vue
+++ ecolomind/src/views/InscriptionView.vue
@@ -4,33 +4,64 @@
         <h1>Inscription</h1>
         <p>Rejoignez la communauté Ecolomind</p>
 
-        <form>
+        <form @submit.prevent="creatUser">
             <div>
-                <label for="pseudo"></label>
-                <input type="text" placeholder="Pseudo" name="" id="pseudo">
+                <label for="username"></label>
+                <input type="text" placeholder="Pseudo" name="" id="username" v-model="formData.username">
             </div>
 
             <div>
                 <label for="email"></label>
-                <input type="text" placeholder="Email" name="" id="email">
+                <input type="text" placeholder="Email" name="" id="email" v-model="formData.email">
             </div>
 
             <div>
                 <label for="password"></label>
-                <input type="password" placeholder="Mot de passe" name="" id="password">
+                <input type="password" placeholder="Mot de passe" name="" id="password" v-model="formData.password">
             </div>
 
             <div>
                 <label for="confirm_password"></label>
-                <input type="password" placeholder="Confirmer le mot de passe" name="" id="confirm_password">
+                <input type="password" placeholder="Confirmer le mot de passe" name="" id="confirm_password" v-model="formData.confirm_password">
             </div>
-            <div>
-                <input type="checkbox" name="" id="">
-                <label for="check-privacy">En m'inscrivant, j'accepte les <router-link :to="{name: 'politique-de-confidentialite'}">
+            <!-- <div>
+                <input type="checkbox" name="" id="checkbox" v-model="formData.checkbox">
+                <label for="checkbox">En m'inscrivant, j'accepte les <router-link :to="{name: 'politique-de-confidentialite'}">
                 Politique de confidentialité
                 </router-link></label>                               
-            </div>
+            </div> -->
             <button>Je m'inscris</button>
         </form>
     </div>
 </template>
+
+<script>
+
+import axios from 'axios';
+
+export default {
+    name : 'InscriptionView',
+    data(){
+        return {
+            formData: {
+                username: '',
+                email: '',
+                password: '',
+                confirm_password: '',
+                // checkbox: '',
+            }, 
+        }
+        
+    },
+    methods: {
+         creatUser(){
+             console.log('debut from data');
+            console.log(JSON.parse(JSON.stringify(this.formData)));
+             console.log('fin from data');
+            axios.post('http://ecolotips.local/wp-json/wp/v2/users', JSON.parse(JSON.stringify(this.formData))) //don't forget LE PIÈGE
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+        }
+    }
+}
+</script>
