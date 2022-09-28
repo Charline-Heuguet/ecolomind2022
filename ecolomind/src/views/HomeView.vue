c345c3f4e2f1db5e31339bf9ffae21ce8add819c
Guillaume-Pou <wlg2012@gmail.com>
2022-09-28 17:46:35 +0200

    wip

--- ecolomind/src/views/HomeView.vue
+++ ecolomind/src/views/HomeView.vue
@@ -9,7 +9,12 @@
     <img id="bureau" src="../assets/bureau.png" alt="">
     <img id="cuisine" src="../assets/cuisine.png" alt="">
     <img id="chambre" src="../assets/chambre.png" alt="">
-  </div>
+    <img id="potager"  src="../assets/potager.png" alt="" />
+    
+ 
+    </div>
+
+  
 
 </template>
 
@@ -31,12 +36,13 @@ export default {
 
 
 #image{
-  max-width: 100%;
+  max-width: 60%;
   background-repeat: no-repeat;
   width: 80%;
   position: relative;
   margin-left: auto;
   margin-right: auto;
+
 }
 #background{
   width: 100%;
@@ -46,56 +52,81 @@ export default {
   position: absolute;
   top: 7.36%;
   left: 27.45%;
-  transform: scale(1);
-  transition: all .6s ;
+ /*  transform: scale(1);
+  transition: all .6s ; */
+ filter:brightness(50%)
+}
+#chambre{
+  width: 27%;
+  position: absolute;
+  top: 38.7%;
+  left: 27.55%;
+ /*  transform: scale(1);
+  transition: all .6s ; */
+  filter:brightness(50%)
 }
 #bureau{
  width: 22.1%;
   position: absolute;
   top: 38.7%;
-  left: 56.09999%;
-  transform: scale(1);
-  transition: all .6s ;
+  left: 56.04%;
+ /*  transform: scale(1);
+  transition: all .6s ; */
+  filter:brightness(50%)
 }
 #entree{
   width: 14.8%;
   position: absolute;
   top: 63.4%;
   left: 27.5%;
-  transform: scale(1);
-  transition: all .6s ;
+ /*  transform: scale(1);
+  transition: all .6s ; */
+  filter:brightness(50%)
 }
 #cuisine {
-  width: 34.3%;
+  width: 34.4%;
   position: absolute;
   top: 63.4%;
-  left: 43.8%;
-  transform: scale(1);
-  transition: all .6s ;
+  left: 43.9%;
+  /* transform: scale(1);
+  transition: all .6s ; */
+  filter:brightness(50%)
 }
-#chambre{
-  width: 27%;
+
+#potager{
+  width: 22%;
   position: absolute;
-  top: 38.7%;
-  left: 27.5%;
-  transform: scale(1);
-  transition: all .6s ;
+  top: 75%;
+  left: 2%;
+ /*  transform: scale(1);
+  transition: all .6s ; */
+  filter:brightness(50%)
+
 }
 #bureau:hover{
-  transform: scale(1.1);
+  /* transform: scale(1.1); */
+  filter: brightness(100%);
+  
 } 
 #sdb:hover{
-  
-  transform: scale(1.1);
+  filter: brightness(100%);
+ /*  transform: scale(1.1); */
 }
 #entree:hover{
-  transform: scale(1.1);
+  filter: brightness(100%);
+  /* transform: scale(1.1); */
 }
 #cuisine:hover{
-  transform: scale(1.1);
+  filter: brightness(100%);
+  /* transform: scale(1.1); */
 }
 #chambre:hover{
-  transform: scale(1.1);
+  filter: brightness(100%);
+/*   transform: scale(1.1); */
+}
+#potager:hover{
+  filter: brightness(100%);
+ /*  transform: scale(1.1); */
 }
 
 
