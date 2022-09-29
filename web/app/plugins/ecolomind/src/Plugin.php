7d813695a0d3baa9dbc6db8a3f977c28bc5b46e4
Guillaume-Pou <wlg2012@gmail.com>
2022-09-29 11:55:45 +0200

    wip

--- web/app/plugins/ecolomind/src/Plugin.php
+++ web/app/plugins/ecolomind/src/Plugin.php
@@ -17,6 +17,16 @@ class Plugin
     {
  //Dans une classe, la fonction add_action à besoin de connaitre, en plus du nom de la fonction à exécuté, le contexte d'execution
  add_action('init', [$this, 'onInit']);
+
+  add_action('rest_api_init', function(){
+
+    register_rest_route('http://ecolomind.local/wp-json/wp/v2/','users_test',[
+        'methods'=>['GET','POST'],
+        'callback'=>'users_test',
+        'permission_callback'=>'__return_true',
+
+    ]);
+ } );
         
  //Lorsque l'on active le plugin on effectue l'action 
  register_activation_hook(ECOLOMIND_PLUGIN_ENTRY, [$this, "onPluginActivation"]);
