5055cbe27fbf6f001c55dc82ad1e4c84a522da33
jeanne-multon <jeanne.multon@gmail.com>
2022-10-04 12:17:20 +0200

    read Comments

--- ecolomind/src/views/SingleTipsView.vue
+++ ecolomind/src/views/SingleTipsView.vue
@@ -7,76 +7,69 @@
         <h3 v-html="this.astuce.title.rendered"></h3>
         <!-- <img src="" alt="">         -->
 
-        <!-- <p v-html="this.astuce._embedded.author.name"></p> -->
+        <p v-html="this.astuce._embedded.author.name"></p>
         <p v-html="this.astuce.content.rendered"></p>
         <!-- <img src="" alt=""> -->
 
-        <h4>Espace commentaires</h4>          
-        <form @click.prevent="submitComment">
-            <label for="comment">Ajoutez votre commentaire :</label><br/>
-            <input type="text" name="comment" id="comment">
-
-            <button type="submit">Envoyez</button>
-        </form>
-
-        <!-- <div>
-            <CommentListComponent 
-                v-for="comment in comments"
-                :key="comment.id"
-                :title="comment.title.rendered"
-                :content="comment.excerpt.rendered"
+        <div>
+            <CommentFormComponent 
+            @commentAdded="this.readComments()"
+            :astuce_id="this.astuce.id"
             />
-        </div> -->
-        
+            <CommentListComponent :comments="this.comments"/>
+        </div>
 
     </div>
 </template>
 
 <script>
-
-// import CommentListComponent from '@/components/CommentListComponent';
+import CommentFormComponent from '@/components/CommentFormComponent';
+import CommentListComponent from '@/components/CommentListComponent';
 import axios from 'axios';
 
-export default({
+export default{
     name: 'SingleTipsView',
-    // components : {
-    //     CommentListComponent,
-    // },
+    components : {
+        CommentListComponent,
+        CommentFormComponent,
+    },
 
     data(){
         return{
             astuce: false,
-            // comments : false,
+            comments : false,
         }
     },
 
-    async created()
+    created()
     {
         const base_url= "http://ecolomind.local/wp-json";
 
         axios.get(base_url + "/wp/v2/tips/"+this.$route.params.id+"?_embed").then((response) => {
             this.astuce = response.data;
             console.log(this.astuce);
+
+            this.readComments();
         })
 
-        // this.readComments();
+        
     },
 
-    // methods: {
+    methods: {
 
-    //     /**
-    //      * Cette fonction est éxectué deux fois. Une fois au moment de la création du composant et une fois à chaque fois qu'un commentaire est ajouté
-    //      */
+        /**
+         * Cette fonction est executée deux fois. Une fois au moment de la création du composant et une fois à chaque fois qu'un commentaire est ajouté
+         */
         
-    //         readComments(){
-    //             const base_url =  "http://ecolomind.local/wp-json";
-    //             axios.get(base_url + "/wp/v2/comments?tips="+this.astuce.id ).then((response) => {
-    //                 this.comments = response.data;
-    //                 console.log(this.comments);
-    //             });
-    //         }
-    // }
-})
+            readComments(){
+                const base_url =  "http://ecolomind.local/wp-json";
+                axios.get(base_url + "/wp/v2/comments?post="+this.astuce.id ).then((response) => {
+                    this.comments = response.data;
+                    console.log(this.comments);
+                });
+            }
+    }
+}
 </script>
 
 
