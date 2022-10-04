656b5fefb5a49f9b1e14e1bcc910b783f65d0c9d
jeanne-multon <jeanne.multon@gmail.com>
2022-10-04 16:33:49 +0200

    tools dynamic

--- ecolomind/src/views/SingleTipsView.vue
+++ ecolomind/src/views/SingleTipsView.vue
@@ -19,6 +19,18 @@
                 </li>
             </ul>
         </div>
+
+        <div class="ingredients">
+            <span>Ustensiles :</span>
+            <ul>
+                <li 
+                    v-for="tool of this.tools"
+                    :key="tool.id"
+                    v-html="tool.name"
+                    >  
+                </li>
+            </ul>
+        </div>
         
         <p v-html="this.astuce.content.rendered"></p>
         <!-- <img src="" alt=""> -->
@@ -51,6 +63,7 @@ export default{
             astuce: false,
             ingredients: false,
             comments : false,
+            tools : false,
         }
     },
 
@@ -60,12 +73,15 @@ export default{
 
         axios.get(base_url + "/wp/v2/tips/"+this.$route.params.id+"?_embed").then((response) => {
             this.astuce = response.data;
-            console.log(this.astuce);
+            // console.log(this.astuce);
 
             axios.get(base_url + "/wp/v2/ingredients?post="+this.astuce.id ).then((response) => {
                 this.ingredients = response.data;
             });
-            console.log(this.ingredients);
+
+            axios.get(base_url + "/wp/v2/tools?post="+this.astuce.id ).then((response) => {
+                this.tools = response.data;
+            });
 
             this.readComments();
         })
