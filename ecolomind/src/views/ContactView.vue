1631510ea1ee03579fb257c6c371ee3a950a1017
Charline-Heuguet <charline.heuguet@gmail.com>
2022-10-07 00:29:31 +0200

    form is ok

--- ecolomind/src/views/ContactView.vue
+++ ecolomind/src/views/ContactView.vue
@@ -1,1 +1,93 @@
 <!-- Page de contact -->
+<template>
+
+    <h3> Vous désirez nous poser une question ? Nous suggérer une amélioration ?</h3>
+    <h3>Contactez-nous !</h3> 
+
+    <form>
+
+            <div class="form">
+                <label for="name"> Votre nom (obligatoire)</label>
+                <input v-model="this.nameForm" type="text" id="name"/>
+                <div v-if="this.errors.nameMissing">Pseudo non renseigné</div>
+            </div>
+
+            <div class="form">
+                <label for="mail"> Votre email (obligatoire)</label>
+                <input v-model="this.mailForm" type="text" id="mail" />
+                <div v-if="this.errors.mailMissing"> Mail non renseigné</div>
+            </div>
+
+            <div class="form">
+                <label for="subject"> Sujet (facultatif) </label>
+                <input v-model="this.subjectForm" type="text" id="subject" />
+            </div>
+
+            <div class="form">
+                <label for="message" > Votre message</label>
+                <textarea v-model="this.msgForm" rows="10" cols="50"  > </textarea>
+                <div v-if="this.errors.msgMissing"> Vous n'avez pas écrit de message</div>
+            </div>
+
+            <button v-on:click.prevent="Send()" >Envoyer</button>
+    </form>
+</template>
+
+
+<script>
+export default{
+    name: "ContactView",
+    data(){
+        return {
+            nameForm : "",
+            mailForm : "",
+            subjectForm : "",
+            msgForm: "", 
+            errors : {
+                nameMissing: false,
+                mailMissing : false,
+                msgMissing : false,
+            }
+        }
+    },
+    methods: {
+        async Send(){
+            this.errors.nameMissing = false;
+            this.errors.msgMissing = false;
+            this.errors.mailMissing = false;
+            //form with error
+            if(this.nameForm == ""){
+                this.errors.nameMissing = true;
+            }
+            if(this.mailForm == ""){
+                this.errors.mailMissing = true;
+            }
+            if(this.msgForm == ""){
+                this.errors.msgMissing = true;
+            }
+            //form without error
+            // if(!this.errors.nameMissing && !this.errors.mailMissing &&!this.errors.msgMissing){
+            //     const sendMessage = await ;
+            // }
+        }
+    }
+}
+//import axios from 'axios';
+</script>
+
+<style lang="scss">
+
+input{
+    height: 30px;
+    width: 250px;
+    margin: 5px auto 20px;
+}
+label, input {
+    display: block;
+}
+
+button, h3{
+    margin-top: 20px;
+    margin-bottom: 30px;
+}
+</style>
