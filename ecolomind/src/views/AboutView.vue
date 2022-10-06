62a901b011d4e25ad1a8579e10e3614d08d7e5d0
fabio-chaher <fabio.chaher@gmail.com>
2022-10-06 15:42:08 +0200

    WIP team-cards

--- ecolomind/src/views/AboutView.vue
+++ ecolomind/src/views/AboutView.vue
@@ -1,8 +1,54 @@
 <!-- Page a propos  -->
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores culpa dolorem vero repellat tempore, earum facere saepe consectetur, similique perferendis cupiditate! Odit repudiandae, maxime molestias saepe qui obcaecati facere tenetur.</p>
-  </div>
+<main class= "contact">
+
+          <div class="card">
+              <div class="card-image"></div>
+              <h2>Prénom</h2>
+              <h2>Nom</h2>
+              <h2>Role</h2>
+          </div>  
+
+        
+          <div class="card">
+            <div class="card-image"></div>
+            <h2>Prénom</h2>
+            <h2>Nom</h2>
+            <h2>Role</h2>
+          </div> 
+
+       
+          <div class="card">
+            <div class="card-image"></div>
+            <h2>Prénom</h2>
+            <h2>Nom</h2>
+            <h2>Role</h2>
+          </div> 
+
+        
+          <div class="card">
+            <div class="card-image"></div>
+            <h2>Prénom</h2>
+            <h2>Nom</h2>
+            <h2>Role</h2>
+          </div> 
+
+       
+          <div class="card">
+            <div class="card-image"></div>
+            <h2>Prénom</h2>
+            <h2>Nom</h2>
+            <h2>Role</h2>
+          </div> 
+
+           <div class="card">
+            <div class="card-image"></div>
+            <h2>Prénom</h2>
+            <h2>Nom</h2>
+            <h2>Role</h2>
+          </div> 
+
+  </main>
+
 </template>
