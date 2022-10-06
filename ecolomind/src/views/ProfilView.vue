f1eecf835cf20ef83c29047854d51de8af11208b
jeanne-multon <jeanne.multon@gmail.com>
2022-10-06 11:10:53 +0200

    modification des données du profil

--- ecolomind/src/views/ProfilView.vue
+++ ecolomind/src/views/ProfilView.vue
@@ -2,29 +2,81 @@
 
 
 <template>
-    <form>
-        <div>
-            <label for="pseudo"></label>
-            <input type="text" placeholder="Gerceval" id="pseudo" />
-        </div>
 
-        <div>
-            <label for="email"></label>
-            <input type="text"  placeholder="gerceval@legallois.com" id="email" />
-        </div>
+    <div>
 
-        <div>
-            <label for="password"></label>
-            <input type="password" placeholder="******" id="password" />
-        </div>
+        <h3>Modifier mes informations :</h3>
 
-        <button>Enregistrer les modifications</button>
-    </form>
+        <form @submit.prevent="updateMyInfos">
+            <div>
+                <label for="pseudo"></label>
+                <input type="text" placeholder="Gerceval" id="pseudo" name="pseudo" v-model="formData.pseudo"/>
+            </div>
+
+            <div>
+                <label for="email"></label>
+                <input type="text"  placeholder="gerceval@legallois.com" id="email" name="email" v-model="formData.email"/>
+            </div>
+
+            <div>
+                <label for="password"></label>
+                <input type="password" placeholder="******" id="password" name="password" v-model="formData.password"/>
+            </div>
+
+            <button>Enregistrer les modifications</button>
+        </form>
+    </div>
+
+    
 </template>
 
 
 <script>
+
+import axios from 'axios';
 export default {
     name: 'ProfilView',
+
+    data(){
+        return{
+
+            usersInfos: {},
+
+            formData: {
+                pseudo: "",
+                email: "",
+                password: "",
+            },
+        }
+    },
+
+    async created() {
+        this.usersInfos = await this.getUsersInfos();
+        console.log(this.usersInfos);
+        console.log("là");
+    },
+
+    methods:
+    {
+        async updateMyInfos(){
+            console.log(JSON.parse(JSON.stringify(this.formData)));
+            axios.put('http://ecolomind.local/wp-json/wp/v2/ecolomind/profil', JSON.parse(JSON.stringify(this.formData)))
+            .then(response => console.log(response))
+            .catch(function(){
+                return {data: null}
+            })
+        },
+
+        async getUsersInfos(){
+            this.usersInfos = await axios.get('http://ecolomind.local/wp-json/wp/v2/ecolomind/profil', JSON.parse(JSON.stringify(this.usersInfos)))
+            .then(response => console.log(response))
+            .catch(function(){
+                return {data: null}
+            })
+
+        },
+
+        
+    },
 }
 </script>
