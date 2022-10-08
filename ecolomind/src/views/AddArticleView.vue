454af2f7e5e8e7050c5d5b7774b4c923735e1a24
jeanne-multon <jeanne.multon@gmail.com>
2022-10-08 16:01:31 +0200

    navbar résolue, la page proposer une astuce était incomplète

--- ecolomind/src/views/AddArticleView.vue
+++ ecolomind/src/views/AddArticleView.vue
@@ -4,48 +4,45 @@
 
     <div id="add-article">
         
-        <h3> Ajouter une astuce </h3>
+        <h3> Ajouter une astuce</h3>
 
-        <form>
+        <form @submit.prevent="createTips">
 
-            <select name="rooms" id="room-select" v-model="selected" >
-                <option v-for="option in options"  :key="option.value">
-                    
+            <div >
+                <select name="rooms" id="room-select" v-model="this.formData.selectedRoom">
+                    <option v-for="room in this.rooms" :key="room.id" :value="room.id">
+                        {{room.name}}
                     </option>
+                </select>
+            </div>
+            
+            <select name="difficulty" id="difficulty-select" v-model="this.formData.selectedDifficulty">
+                <option v-for="difficulty in this.difficulties" :key="difficulty.id" :value="difficulty.id">
+                    {{difficulty.name}}
+                </option>
             </select>
 
-          <!-- <div>
-                <select name="difficulty" id="difficulty-select" v-model="formData.difficulty">
-                    <option value="" style="display: none">Quel est le niveau de difficulté? </option>
-                    <option value="">Facile</option>
-                    <option value="">Moyen</option>
-                    <option value="">Difficile</option>
-                </select>                
-            </div>  -->
-
             <div>
-                <label for="title"></label>
-                <input type="text" id="title" name="title" placeholder="Titre de votre astuce" v-model="formData.title">
+                <label for="title">Inscrire le titre de votre astuce :</label>
+                <input type="text" id="title" name="title" placeholder="Titre de votre astuce" v-model="formData.titre">
             </div>
-
 
             <div>
                 <label for="ingredients"></label>
-                <input type="text" id="ingredients" name="ingredients" placeholder="Les ingrédients nécessaires" v-model="formData.ingredients">
+                <input type="text" id="ingredients" name="ingredients" placeholder="Les ingrédients nécessaires" v-model="formData.ingredient">
             </div>
 
             <div>
                 <label for="content"></label>
-                <input name="content" id="content" placeholder="Ecrivez les quantités des ingredients nécessaires à votre astuce.Rédigez votre recette en la détaillant" v-model="formData.content">
+                <input type="textarea" name="content" id="content" placeholder="Ecrivez les quantités des ingredients nécessaires à votre astuce.Rédigez votre recette en la détaillant" v-model="formData.contenu">
             </div>
 
             <div>
                 <label for="photo">Postez la photo de votre produit</label>
-                <input type="file" id="photo" name="photo" accept="image/png, image/jpeg" v-change="formData.photo">
+                <input type="file" id="photo" name="photo" accept="image/png, image/jpeg">
             </div>
 
-
-            <button type="submit">Envoyez votre tips</button>
+            <button>Envoyez votre astuce</button>
 
 
         </form>
@@ -56,46 +53,66 @@
 
 <script>
 
+import axios from 'axios';
+import TipsServices from '@/services/TipsServices.js';
 
 
 export default {
-
+ 
     name: "AddArticleView",
 
 
     data(){
         return {
+            difficulties: [],
+            rooms: [],
+
             formData: {
-                rooms: '',
-                difficulty: '',
-                title: '',
-                ingredients: '',
+                selectedDifficulty: "",
+                selectedRoom: "",
+                titre: '',
+                ingredient: '',
+                contenu: "",
                 photo:"",
                
             }, 
-           
-            
         }
         
     },
+
+    async created(){
+
+        // this.difficulties = await TipsServices.getDifficultyById();
+        this.difficulties = await TipsServices.getAllDifficulties();
+        // console.log(this.formData.difficulties);
+
+        this.rooms = await TipsServices.getAllRooms();
+
+    },
     methods:
-    
-    
-    {
-             
+    {   
          async createTips(){
     
-           /* console.log(JSON.parse(JSON.stringify(this.formData)));
-             console.log('fin from data');
-            axios.post('http://ecolomind.local/wp-json/wp/v2/ecolomind/tips', JSON.parse(JSON.stringify(this.formData)),this.$router.push({name : "connexion"})) //don't forget LE PIÈGE
+            // console.log(this.formData.selectedDifficulty);
+            console.log(JSON.parse(JSON.stringify(this.formData)));
+            //  console.log('fin from data');
+            axios.post('http://ecolomind.local/wp-json/wp/v2/ecolomind/addTips', JSON.parse(JSON.stringify(this.formData))) //don't forget LE PIÈGE
              .then(response => console.log(response)) 
             .catch(function(){ 
-               
-            // on veut éviter d'interrompre l'exécution JS ce que pourrait faire une erreur 403 dans axios, on va donc pour contrer ça renvoyer un objet null
-            return {data: null}
-        })*/
-        } 
+                
+                // on veut éviter d'interrompre l'exécution JS ce que pourrait faire une erreur 403 dans axios, on va donc pour contrer ça renvoyer un objet null
+                return {data: null}
+            })
+        },
+
+        // debug(){
+        //     console.log(this.selectedDifficulty);
+        // }
+
+        // onChange:function(evenement){
+        //     console.log(evenement.target.value);
+        // }
         
-    }
+    },
 }
 </script>
