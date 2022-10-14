25e5a6727a41f4c8a953167ffd4a517e07682086
AlexandreQuemeneur <alexandre.quemeneur17@gmail.com>
2022-10-14 17:03:29 +0200

    card responsive

--- ecolomind/src/components/TipsCardComponent.vue
+++ ecolomind/src/components/TipsCardComponent.vue
@@ -53,20 +53,28 @@ export default({
 <style lang="scss" scoped>
 
 article{
-    background-color: rgba($color: #FFCED8, $alpha: 0.5);
+    background-color: #c7d3e3;
     border-radius: 15px;
     flex: 0 1 32%; //flex: capacité à s'étendre, capacité à se réduire et la largeur
-    text-align: center;
+    text-align: left;
     padding: 20px 20px 0px;
     margin: 10px 0;
-    
+    width: 50vh;
     h2{
         text-align: left;
         margin: 0;
     }
+    img{
+        float:right;
+        margin-bottom: 5px;
+    }
+    h2{
+        clear:right;
+    }
+    #suite{
+        
+    }
 }
-
-
 .try{
     max-width: 50px;
 }
