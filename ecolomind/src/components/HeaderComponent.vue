cbc1fae8c57cb9acc9ffe5244caca1c2640bb676
AlexandreQuemeneur <alexandre.quemeneur17@gmail.com>
2022-10-16 13:52:03 +0200

    full mobile

--- ecolomind/src/components/HeaderComponent.vue
+++ ecolomind/src/components/HeaderComponent.vue
@@ -3,11 +3,11 @@
           
         <div class="user-state" v-if="this.$store.state.userIsConnect">
             <img src="@/assets/ecolominds-logo.png" class="logo" alt="logo-ecolomind">   
-            <!-- <div class="elements"> -->
+            <div class="elements">
                 <span>Bonjour : {{ getCurrentUserName() }}</span>
                 <router-link to="/profil">Profil</router-link>
                 <a href="#" @click="this.$store.dispatch('onUserDisconnection') + RemoveToken()">Deconnexion</a>  
-            <!-- </div>               -->
+            </div>              
         </div>
         
         <div class="user-state" v-if="!this.$store.state.userIsConnect">
@@ -74,13 +74,7 @@ export default {
    display: flex; 
    flex-direction: column;
 }
-// .user-state{
-//     box-sizing: border-box;
-//     border-radius: 30px;
-//     box-shadow: 0px 5px 5px 5px rgba(62,66,66,0.22);
-//     align-items: center;
-//     width: 35vh;  
-// }
+
 a{
     color: #2c3e50;
     &.router-link-exact-active {
@@ -91,6 +85,35 @@ header{
     display: flex;
     justify-content: space-between;
     margin: 2vh 0 0 2vh ;
+}
+@media screen and (min-width: 700px){ 
+    .logo{
+       max-height: 140px; 
+    }
+    .user-state{
+        display: flex;
+        align-items: center;
+        box-sizing: border-box;
+        border-radius: 30px;
+        background: linear-gradient(180deg, rgb(105, 177, 219) 0%, rgba(255,255,255,1) 100%);
+        box-shadow: 0px 5px 5px 5px rgba(62,66,66,0.22);
+        align-items: center;
+        width: 35vh;  
+    }
+    .dark-theme{
+        .user-state{
+            background: url('~@/assets/stars.png'), linear-gradient(180deg, #053979e0 0%, #5c6269a6 100%);
+            .elements{
+                a{
+                    color: #6c6f74;
+                    &.router-link-exact-active {
+                        color: #42b983;
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 
