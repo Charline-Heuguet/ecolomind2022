3caa4dc98889e4587fadae612699b2f2fdc6d8be
jeanne-multon <jeanne.multon@gmail.com>
2022-10-17 02:32:09 +0200

    link back to home

--- ecolomind/src/views/404View.vue
+++ ecolomind/src/views/404View.vue
@@ -1,8 +1,13 @@
 
 <template>
     <div class="wrong-path">
-        <h2>Bah, alors ! Qu'est-ce que tu fais là ?</h2>
-        <img src="../assets/guillaume_Ferard.jpg" alt="You Shall Not Passsss !">
+        <h2>Je crois que tu t'es perdu...</h2>
+        <router-link to="/">
+            <img src="../assets/guillaume_Ferard.jpg" alt="You Shall Not Passsss !">
+        </router-link>
+        <router-link to="/">
+            <h3>Allez, reviens à la maison !</h3>
+        </router-link>
     </div>
 </template>
 
