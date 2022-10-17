a0df2fd46bfe304bcf404ce385ddc7badfa73142
jeanne-multon <jeanne.multon@gmail.com>
2022-10-17 02:17:36 +0200

    error 404 without css

--- ecolomind/src/views/404View.vue
+++ ecolomind/src/views/404View.vue
@@ -1,7 +1,8 @@
 
 <template>
-    <div>
-        hey page not found !!!
+    <div class="wrong-path">
+        <h2>Bah, alors ! Qu'est-ce que tu fais là ?</h2>
+        <img src="../assets/guillaume_Ferard.jpg" alt="You Shall Not Passsss !">
     </div>
 </template>
 
